45ac0cbdf59e7a355ab4761d3550dc0b79a4522c
alperenyazmaci <38111663+alperenyazmaci@users.noreply.github.com>
2024-06-02 21:02:35 -0400

    Update vm.c

--- COP3402/HW1/vm.c
+++ COP3402/HW1/vm.c
@@ -29,14 +29,14 @@ int BP = 499;
 int SP = 500;
 int PC = 10;
 Instruction IR;
-int STACK[ARRAY_SIZE] = {0};
+int SAP[ARRAY_SIZE] = {0};
 int HALT = 1;
 
 //Function to Find base L levels down
 int base(int BP, int L) {
     int arb = BP;
     while (L > 0) {
-        arb = STACK[arb];
+        arb = SAP[arb];
         L--;
     }
     return arb;
@@ -51,9 +51,9 @@ void print(const char instr_name[], int L, int M) {
     for (int i = 499; i >= SP; i--) {
         if (i == current_bp && current_bp != 499) {
             printf("| ");
-            current_bp = STACK[current_bp + 1];
+            current_bp = SAP[current_bp + 1];
         }
-        printf("%d ", STACK[i]);
+        printf("%d ", SAP[i]);
     }
     printf("\n");
 }
@@ -70,23 +70,23 @@ int main(int argc, char *argv[]) {
         return 1;
     }
 
-    //load instructions into the STACK starting at index 10 then close the file after all instructions are loaded
+    //load instructions into the SAP starting at index 10 then close the file after all instructions are loaded
     int index = 10;
-    while (fscanf(file, "%d %d %d", &STACK[index], &STACK[index + 1], &STACK[index + 2]) != EOF) {
+    while (fscanf(file, "%d %d %d", &SAP[index], &SAP[index + 1], &SAP[index + 2]) != EOF) {
         index += 3;
     }
     fclose(file);
 
     //print the header output lines
-    printf("%15s %5s %5s %5s %10s\n", "", "PC", "BP", "SP", "Stack");
+    printf("%15s %5s %5s %5s %10s\n", "", "PC", "BP", "SP", "SAP");
     printf("Initial values: %5d %6d %5d\n", PC, BP, SP);
 
-    //loop to fetch and execute instructions in the stack
+    //loop to fetch and execute instructions in the SAP
     while (HALT) {
         //fetch cycle
-        IR.OP = STACK[PC];
-        IR.L = STACK[PC + 1];
-        IR.M = STACK[PC + 2];
+        IR.OP = SAP[PC];
+        IR.L = SAP[PC + 1];
+        IR.M = SAP[PC + 2];
         PC += 3;
 
         //determining instruction name then assigning it to instr_name
@@ -108,69 +108,69 @@ int main(int argc, char *argv[]) {
         switch (IR.OP) {
             case 1: //LIT 0 M
                 SP--;
-                STACK[SP] = IR.M;
+                SAP[SP] = IR.M;
                 break;
             case 2: //OPR 0 M
                 switch (IR.M) {
                     case 0: //RTN
                         SP = BP + 1;
-                        BP = STACK[SP - 2];
-                        PC = STACK[SP - 3];
+                        BP = SAP[SP - 2];
+                        PC = SAP[SP - 3];
                         break;
                     case 1: //ADD
-                        STACK[SP + 1] = STACK[SP + 1] + STACK[SP];
+                        SAP[SP + 1] = SAP[SP + 1] + SAP[SP];
                         SP++;
                         break;
                     case 2: //SUB
-                        STACK[SP + 1] = STACK[SP + 1] - STACK[SP];
+                        SAP[SP + 1] = SAP[SP + 1] - SAP[SP];
                         SP++;
                         break;
                     case 3: //MUL
-                        STACK[SP + 1] = STACK[SP + 1] * STACK[SP];
+                        SAP[SP + 1] = SAP[SP + 1] * SAP[SP];
                         SP++;
                         break;
                     case 4: //DIV
-                        STACK[SP + 1] = STACK[SP + 1] / STACK[SP];
+                        SAP[SP + 1] = SAP[SP + 1] / SAP[SP];
                         SP++;
                         break;
                     case 5: //EQL
-                        STACK[SP + 1] = STACK[SP + 1] == STACK[SP];
+                        SAP[SP + 1] = SAP[SP + 1] == SAP[SP];
                         SP++;
                         break;
                     case 6: //NEQ
-                        STACK[SP + 1] = STACK[SP + 1] != STACK[SP];
+                        SAP[SP + 1] = SAP[SP + 1] != SAP[SP];
                         SP++;
                         break;
                     case 7: //LSS
-                        STACK[SP + 1] = STACK[SP + 1] < STACK[SP];
+                        SAP[SP + 1] = SAP[SP + 1] < SAP[SP];
                         SP++;
                         break;
                     case 8: //LEQ
-                        STACK[SP + 1] = STACK[SP + 1] <= STACK[SP];
+                        SAP[SP + 1] = SAP[SP + 1] <= SAP[SP];
                         SP++;
                         break;
                     case 9: //GTR
-                        STACK[SP + 1] = STACK[SP + 1] > STACK[SP];
+                        SAP[SP + 1] = SAP[SP + 1] > SAP[SP];
                         SP++;
                         break;
                     case 10: //GEQ
-                        STACK[SP + 1] = STACK[SP + 1] >= STACK[SP];
+                        SAP[SP + 1] = SAP[SP + 1] >= SAP[SP];
                         SP++;
                         break;
                 }
                 break;
             case 3: //LOD L M
                 SP--;
-                STACK[SP] = STACK[base(BP, IR.L) - IR.M];
+                SAP[SP] = SAP[base(BP, IR.L) - IR.M];
                 break;
             case 4: //STO L M
-                STACK[base(BP, IR.L) - IR.M] = STACK[SP];
+                SAP[base(BP, IR.L) - IR.M] = SAP[SP];
                 SP++;
                 break;
             case 5: //CAL L M
-                STACK[SP - 1] = base(BP, IR.L);
-                STACK[SP - 2] = BP;
-                STACK[SP - 3] = PC;
+                SAP[SP - 1] = base(BP, IR.L);
+                SAP[SP - 2] = BP;
+                SAP[SP - 3] = PC;
                 BP = SP - 1;
                 PC = IR.M;
                 break;
@@ -181,7 +181,7 @@ int main(int argc, char *argv[]) {
                 PC = IR.M;
                 break;
             case 8: //JPC 0 M
-                if (STACK[SP] == 0) {
+                if (SAP[SP] == 0) {
                     PC = IR.M;
                 }
                 SP++;
@@ -189,13 +189,13 @@ int main(int argc, char *argv[]) {
             case 9: //SYS 0 M
                 switch (IR.M) {
                     case 1:
-                        printf("Output result is: %d\n", STACK[SP]);
+                        printf("Output result is: %d\n", SAP[SP]);
                         SP++;
                         break;
                     case 2:
                         SP--;
                         printf("Please enter an integer: ");
-                        scanf("%d", &STACK[SP]);
+                        scanf("%d", &SAP[SP]);
                         break;
                     case 3:
                         HALT = 0;
