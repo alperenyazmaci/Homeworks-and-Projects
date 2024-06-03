2095199753533e2c161589a616f0f1119f39ac2f
alperenyazmaci <38111663+alperenyazmaci@users.noreply.github.com>
2024-06-03 12:08:02 -0400

    a

--- COP3402/HW1/vm.c
+++ COP3402/HW1/vm.c
@@ -29,14 +29,14 @@ int BP = 499;
 int SP = 500;
 int PC = 10;
 Instruction IR;
-int SAP[ARRAY_SIZE] = {0};
+int PAS[ARRAY_SIZE] = {0};
 int HALT = 1;
 
 //Function to Find base L levels down
 int base(int BP, int L) {
     int arb = BP;
     while (L > 0) {
-        arb = SAP[arb];
+        arb = PAS[arb];
         L--;
     }
     return arb;
@@ -51,9 +51,9 @@ void print(const char instr_name[], int L, int M) {
     for (int i = 499; i >= SP; i--) {
         if (i == current_bp && current_bp != 499) {
             printf("| ");
-            current_bp = SAP[current_bp + 1];
+            current_bp = PAS[current_bp + 1];
         }
-        printf("%d ", SAP[i]);
+        printf("%d ", PAS[i]);
     }
     printf("\n");
 }
@@ -70,23 +70,23 @@ int main(int argc, char *argv[]) {
         return 1;
     }
 
-    //load instructions into the SAP starting at index 10 then close the file after all instructions are loaded
+    //load instructions into the PAS starting at index 10 then close the file after all instructions are loaded
     int index = 10;
-    while (fscanf(file, "%d %d %d", &SAP[index], &SAP[index + 1], &SAP[index + 2]) != EOF) {
+    while (fscanf(file, "%d %d %d", &PAS[index], &PAS[index + 1], &PAS[index + 2]) != EOF) {
         index += 3;
     }
     fclose(file);
 
     //print the header output lines
-    printf("%15s %5s %5s %5s %10s\n", "", "PC", "BP", "SP", "SAP");
+    printf("%15s %5s %5s %5s %10s\n", "", "PC", "BP", "SP", "PAS");
     printf("Initial values: %5d %6d %5d\n", PC, BP, SP);
 
-    //loop to fetch and execute instructions in the SAP
+    //loop to fetch and execute instructions in the PAS
     while (HALT) {
         //fetch cycle
-        IR.OP = SAP[PC];
-        IR.L = SAP[PC + 1];
-        IR.M = SAP[PC + 2];
+        IR.OP = PAS[PC];
+        IR.L = PAS[PC + 1];
+        IR.M = PAS[PC + 2];
         PC += 3;
 
         //determining instruction name then assigning it to instr_name
@@ -108,69 +108,69 @@ int main(int argc, char *argv[]) {
         switch (IR.OP) {
             case 1: //LIT 0 M
                 SP--;
-                SAP[SP] = IR.M;
+                PAS[SP] = IR.M;
                 break;
             case 2: //OPR 0 M
                 switch (IR.M) {
                     case 0: //RTN
                         SP = BP + 1;
-                        BP = SAP[SP - 2];
-                        PC = SAP[SP - 3];
+                        BP = PAS[SP - 2];
+                        PC = PAS[SP - 3];
                         break;
                     case 1: //ADD
-                        SAP[SP + 1] = SAP[SP + 1] + SAP[SP];
+                        PAS[SP + 1] = PAS[SP + 1] + PAS[SP];
                         SP++;
                         break;
                     case 2: //SUB
-                        SAP[SP + 1] = SAP[SP + 1] - SAP[SP];
+                        PAS[SP + 1] = PAS[SP + 1] - PAS[SP];
                         SP++;
                         break;
                     case 3: //MUL
-                        SAP[SP + 1] = SAP[SP + 1] * SAP[SP];
+                        PAS[SP + 1] = PAS[SP + 1] * PAS[SP];
                         SP++;
                         break;
                     case 4: //DIV
-                        SAP[SP + 1] = SAP[SP + 1] / SAP[SP];
+                        PAS[SP + 1] = PAS[SP + 1] / PAS[SP];
                         SP++;
                         break;
                     case 5: //EQL
-                        SAP[SP + 1] = SAP[SP + 1] == SAP[SP];
+                        PAS[SP + 1] = PAS[SP + 1] == PAS[SP];
                         SP++;
                         break;
                     case 6: //NEQ
-                        SAP[SP + 1] = SAP[SP + 1] != SAP[SP];
+                        PAS[SP + 1] = PAS[SP + 1] != PAS[SP];
                         SP++;
                         break;
                     case 7: //LSS
-                        SAP[SP + 1] = SAP[SP + 1] < SAP[SP];
+                        PAS[SP + 1] = PAS[SP + 1] < PAS[SP];
                         SP++;
                         break;
                     case 8: //LEQ
-                        SAP[SP + 1] = SAP[SP + 1] <= SAP[SP];
+                        PAS[SP + 1] = PAS[SP + 1] <= PAS[SP];
                         SP++;
                         break;
                     case 9: //GTR
-                        SAP[SP + 1] = SAP[SP + 1] > SAP[SP];
+                        PAS[SP + 1] = PAS[SP + 1] > PAS[SP];
                         SP++;
                         break;
                     case 10: //GEQ
-                        SAP[SP + 1] = SAP[SP + 1] >= SAP[SP];
+                        PAS[SP + 1] = PAS[SP + 1] >= PAS[SP];
                         SP++;
                         break;
                 }
                 break;
             case 3: //LOD L M
                 SP--;
-                SAP[SP] = SAP[base(BP, IR.L) - IR.M];
+                PAS[SP] = PAS[base(BP, IR.L) - IR.M];
                 break;
             case 4: //STO L M
-                SAP[base(BP, IR.L) - IR.M] = SAP[SP];
+                PAS[base(BP, IR.L) - IR.M] = PAS[SP];
                 SP++;
                 break;
             case 5: //CAL L M
-                SAP[SP - 1] = base(BP, IR.L);
-                SAP[SP - 2] = BP;
-                SAP[SP - 3] = PC;
+                PAS[SP - 1] = base(BP, IR.L);
+                PAS[SP - 2] = BP;
+                PAS[SP - 3] = PC;
                 BP = SP - 1;
                 PC = IR.M;
                 break;
@@ -181,7 +181,7 @@ int main(int argc, char *argv[]) {
                 PC = IR.M;
                 break;
             case 8: //JPC 0 M
-                if (SAP[SP] == 0) {
+                if (PAS[SP] == 0) {
                     PC = IR.M;
                 }
                 SP++;
@@ -189,13 +189,13 @@ int main(int argc, char *argv[]) {
             case 9: //SYS 0 M
                 switch (IR.M) {
                     case 1:
-                        printf("Output result is: %d\n", SAP[SP]);
+                        printf("Output result is: %d\n", PAS[SP]);
                         SP++;
                         break;
                     case 2:
                         SP--;
                         printf("Please enter an integer: ");
-                        scanf("%d", &SAP[SP]);
+                        scanf("%d", &PAS[SP]);
                         break;
                     case 3:
                         HALT = 0;
